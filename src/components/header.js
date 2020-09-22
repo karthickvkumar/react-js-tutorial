afad3310517b677acebb4328081f37da4e32fcff
Karthick Kumar V <karthick.velraj@fpsinc.com>
2020-09-22 22:44:56 +0530

    update

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
 const Header = () => {
+    let username = 'Mr.John';
+    let user = {
+        name: 'Mr.Jim',
+        age: 22,
+        location: 'chennai'
+    }
+
     return (
         <div>
-            <h1>Welcome to Header Page</h1>
+            <h1>Welcome to Home, {username}</h1>
+            <h2>The user name is {user.name}</h2>
+            <h2>The user age is {user.age}</h2>
+            <h2>The user location is {user.location}</h2>
         </div>
     );
 };
