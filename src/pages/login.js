2985e3bd3fb00f74bfd7b2e1136d3d19ca2631c9
kvelraj_concord <kvelraj@concord.net>
2020-10-05 22:26:41 +0530

    update

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,30 +1,39 @@
 import React, { Component } from 'react';
 
 class Login extends Component {
-    constructor(props) {
+    constructor(props){
         super(props);
         this.state = {
-            username: 'Mr.JOHN'
+            username: '',
+            password: ''
         }
     }
 
-    onLogin() {
+    handleInput = (event) => {
         this.setState({
-            username: 'Miss.Jimmy'
+            [event.target.name] : event.target.value
         })
+    }
+
+    onLogin(){
+        console.log(this.state)
     }
 
     render() {
         return (
             <div>
-                <h3 className="message">Welcome to Login Page, {this.state.username}</h3>
+                {/* <h3 className="message">Welcome to Login Page, {this.state.username}</h3> */}
                 <label>Enter your Email:</label>
-                <input type="text" placeholder="Enter your email.." />
+                <input type="text" placeholder="Enter your email.." name="username" onChange={this.handleInput}/>
                 <br></br>
                 <label>Enter your Password:</label>
-                <input type="password" placeholder="Enter your password" />
+                <input type="password" placeholder="Enter your password" name="password" onChange={this.handleInput}/>
                 <br></br>
                 <button onClick={() => this.onLogin()}>Login</button>
+                <div>
+                    <h3>The user email is {this.state.username}</h3>
+                    <h3>The user password is {this.state.password}</h3>
+                </div>
             </div>
         );
     }
