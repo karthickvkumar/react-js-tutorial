b6695fd72e4cfd1fe36fff45bed7cc4cb2416fbe
kvelraj_concord <kvelraj@concord.net>
2020-10-20 22:33:08 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 import Register from './pages/register';
 import Login from './pages/login';
+import ListPage from './pages/list';
 
 
 import './index.css';
 
 let App = () => {
   return (
-  <div>
-    <Login></Login>
-  </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Login}></Route>
+        <Route path="/register" component={Register}></Route>
+        <Route path="/list" component={ListPage}></Route>
+      </Switch>
+    </BrowserRouter>
   )
 }
 
